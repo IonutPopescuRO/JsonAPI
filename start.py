e570cb94da20471d21885c23c3496f2417a42df0
Ionuț Popescu <ionutpopescu@live.com>
2020-04-08 14:48:32 +0300

    The first interaction with json.

--- start.py
+++ start.py
@@ -1,1 +1,28 @@
-print("Hello world!")
+import flask
+from flask import jsonify
+import json
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+def get_json():
+    data = None
+    with open('db/products.json') as json_file:
+        data = json.load(json_file)
+    return data
+
+
+@app.route('/', methods=['GET'])
+def home():
+    products = get_json()
+    return jsonify(products)
+
+
+@app.route('/api', methods=['GET'])
+def api():
+    products = get_json()
+    return jsonify(products)
+
+
+app.run(port=8080)
